df72bae77ae7ca42b5d6eb083deadc56e8a37e1d
utsocials <utsocials@gmail.com>
2021-08-22 02:41:30 -0700

    update firebase to get access to the data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC_7NofSDb1P6uSdCkGfHTK8e1IkEwXM4E",
+    authDomain: "empleados-8655b.firebaseapp.com",
+    projectId: "empleados-8655b",
+    storageBucket: "empleados-8655b.appspot.com",
+    messagingSenderId: "603009156416",
+    appId: "1:603009156416:web:0486c49dcdf87b7f8526a5"
+  }
 };
 
 /*
