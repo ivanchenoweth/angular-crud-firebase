2762dabee8e9045e2b82654fce7abc5e82d4b21d
utsocials <utsocials@gmail.com>
2021-08-22 20:51:21 -0700

    read from firebase lit empleados

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-list-empleados',
   templateUrl: './list-empleados.component.html',
-  styleUrls: ['./list-empleados.component.css']
+  styleUrls: ['./list-empleados.component.css'],
 })
 export class ListEmpleadosComponent implements OnInit {
-  
-  constructor() { 
-    
-   }
+  empleados: any[] = [];
+
+  constructor(private _empleadoService: EmpleadoService) {}
 
   ngOnInit(): void {
+    this.getEmpleados();
   }
 
+  getEmpleados() {
+    this._empleadoService.getEmpleados().subscribe((data) => {
+      data.forEach((element: any) => {
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        });
+      });
+      console.log(this.empleados);
+    });
+  }
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 // service to: communicate to backend, reuse code, communication beteen components
 
 @Injectable({
@@ -15,5 +16,12 @@ export class EmpleadoService {
  */
   agregarEmpleado(empleado: any): Promise<any> {
     return this.firestore.collection('empleados').add(empleado);
+  }
+
+  getEmpleados(): Observable<any> {
+    return this.firestore.collection(
+                                      'empleados',
+                                      ref=>ref.orderBy('fechaCreacion','asc')
+                                    ).snapshotChanges();
   }
 }
